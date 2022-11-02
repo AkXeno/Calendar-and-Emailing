aa913145c15e55598b7fb7d07bcdc709c7336563
AkXeno <117261831+AkXeno@users.noreply.github.com>
2022-11-02 18:21:13 +1030

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <AgendaDaymap />
+    <EmailSystem />
+  </div>
+</template>
+
+<script>
+const width = window.innerWidth;
+const height = window.innerHeight;
+import AgendaDaymap from './components/AgendaDaymap.vue'
+import EmailSystem from './components/EmailSystem.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AgendaDaymap,
+    EmailSystem
+  },
+  data() {
+    return {
+      stageSize: {
+        width: width,
+        height: height
+      }
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #3198ff;
+  margin-top: 0px;
+  background-color: #ffffff;
+}
+</style>
